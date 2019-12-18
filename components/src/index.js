69c29e570b6a3dd59eca4ed2b9f74c829d989b0a
saravanan <saravanan@saravanans-MacBook-Pro.local>
2019-12-18 23:48:43 +0530

    reusable component

--- components/src/index.js
+++ components/src/index.js
@@ -7,13 +7,18 @@ import ApprovalCard from './ApprovalCard'
 const App = () =>{
     return (
         <div className="ui container comments">
-            <ApprovalCard />
+            <ApprovalCard>
             <CommentDetail author="Sam" timeAgo="Today at 6.00PM" content="Nice blog post" 
             avatar={faker.image.avatar()} />
+            </ApprovalCard>
+            <ApprovalCard>
             <CommentDetail author="John" timeAgo="Today at 4.30PM" content="I like the post" 
             avatar={faker.image.avatar()} />
+            </ApprovalCard>
+            <ApprovalCard>
             <CommentDetail author="Jane" timeAgo="Yesterday at 8.00PM" content="I like the writing" 
             avatar={faker.image.avatar()} />
+            </ApprovalCard>
         </div>
     );
 }
