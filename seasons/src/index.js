7a1b9c8f7abf0a767da0d28134fce57b85deed5c
saravanan <saravanan@saravanans-MacBook-Pro.local>
2019-12-22 11:04:24 +0530

    handle error message

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -4,15 +4,25 @@ import ReactDOM from 'react-dom'
 class App extends React.Component{
     constructor(props){
         super(props);
-        this.state = { lat : null}
+        this.state = { lat : null, errmsg : ''}
+
+        window.navigator.geolocation.getCurrentPosition(
+            (position) => {
+                this.setState({lat : position.coords.latitude})
+            },
+            (err) => {
+                this.setState({errmsg : err.message})
+            }
+        );
     }
 
     render(){
-        window.navigator.geolocation.getCurrentPosition(
-            (position) => console.log(position),
-            (err) => console.log(err)
+        return (
+            <div>Latitude : {this.state.lat}
+            <br />
+            Error : {this.state.errmsg}
+            </div>
         );
-        return <div>Latitude: </div>;
     }
 }
 
