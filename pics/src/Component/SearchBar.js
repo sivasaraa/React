6b09a0edc907e807139f2af4c8d281bb9185f911
saravanan <saravanan@saravanans-MacBook-Pro.local>
2019-12-28 00:09:52 +0530

    pass parameter from child to parent

--- pics/src/Component/SearchBar.js
+++ pics/src/Component/SearchBar.js
@@ -12,11 +12,16 @@ class SearchBar extends React.Component{
         console.log(event.target.value);
     }
 
+    onSubmitform = (event) => {
+        event.preventDefault();
+        this.props.onSubmit(this.state.term);
+
+    }
 
     render(){
         return (
             <div className="ui segment">
-                <form className="ui form">
+                <form className="ui form" onSubmit={this.onSubmitform}>
                     <div className="field">
                         <label>Image Search</label>
                         <input type="text" value={this.state.term} onChange={(e) => this.setState({'term':e.target.value}) }/>
