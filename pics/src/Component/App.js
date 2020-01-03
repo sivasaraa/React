29f326307978d33ce12797076fa346e4cc0af093
saravanan <saravanan@saravanans-MacBook-Pro.local>
2020-01-04 01:32:28 +0530

    added request

--- pics/src/Component/App.js
+++ pics/src/Component/App.js
@@ -5,11 +5,20 @@ import SearchBar from './SearchBar'
 class App extends React.Component{
 
     onSearchSubmit(term){
-        console.log(term)
+        axios.get('https://api.unsplash.com/search/photos',{
+            params: {
+                query:term
+            },
+            headers:{
+                Authorization:'Client-ID 42b4d66456b35aacbb72dc282664ed9fcebd545e102e82c0af6d946daabbddb7'
+            }
+        }).then((response) => {
+            console.log(response.data.results);
+        })
     }
 
     render(){
-        return (<div className="ui container" style={{'margin-top':'10px'}}>
+        return (<div className="ui container" style={{'marginTop':'10px'}}>
                 <SearchBar onSubmit={this.onSearchSubmit}/>
             </div>);
     }
