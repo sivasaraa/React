1e2fdc64795c58c6664c76a18bfff8595eda777d
saravanan <saravanan@saravanans-MacBook-Pro.local>
2020-01-04 22:45:12 +0530

    images shown

--- pics/src/Component/ImageList.js
+++ pics/src/Component/ImageList.js
@@ -1,8 +1,10 @@
 import React from 'react'
 
 const ImageList = (props) => {
-    console.log(props.images);
-    return <div>ImageList</div>
+    const image = props.images.map(({id,urls,description}) => {
+        return <img key={id} src={urls.regular} alt={description}/>
+    });
+    return <div>{image}</div>;
 }
 
 export default ImageList
