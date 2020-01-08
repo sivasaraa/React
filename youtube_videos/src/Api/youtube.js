0b152fee162957203fb0fa0d469b1c07fdfb1fae
saravanan <saravanan@saravanans-MacBook-Pro.local>
2020-01-08 22:47:33 +0530

    youtube project

---
+++ youtube_videos/src/Api/youtube.js
@@ -0,0 +1,13 @@
+import axios from 'axios'
+
+const KEY = 'AIzaSyDqRax2W9F-RDpxg04UGtSO0CgTxJzDKI4'
+
+export default axios.create({
+    baseURL : 'https://www.googleapis.com/youtube/v3',
+    params : {
+        part : 'snippet',
+        type : 'video',
+        maxResults : 5,
+        key : KEY
+    }
+})
